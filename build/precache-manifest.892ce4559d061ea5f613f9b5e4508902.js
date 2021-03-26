2766916a14d7b7a5a6f5965e19032ec65e7c828e
kaustavkarmakar2 <kaustavkarmakar2@gmail.com>
2021-03-26 23:32:12 +0530

    second  commit

---
+++ build/precache-manifest.892ce4559d061ea5f613f9b5e4508902.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34491815026f80e8dd618ff47ee11fd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4630a9ce0e21126884e",
+    "url": "/static/css/main.00fb0796.chunk.css"
+  },
+  {
+    "revision": "25f4097045a369289792",
+    "url": "/static/js/2.54678a6f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.54678a6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4630a9ce0e21126884e",
+    "url": "/static/js/main.d5279979.chunk.js"
+  },
+  {
+    "revision": "64434318388dfccd9ce5",
+    "url": "/static/js/runtime-main.e3f8525f.js"
+  }
+]);
